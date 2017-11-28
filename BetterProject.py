07228a92f5b00d360c21258fe8c013f809e51d8f
Andy Hou <andy-hou@student.kirkwood.edu>
2017-11-28 14:48:38 +0000

    bohadsf thingy fixed betterproject kinda

--- BetterProject.py
+++ BetterProject.py
@@ -4,42 +4,46 @@ import random
 import sys
 import math
 
-def main():
-        start()
-                
 def start():
-        print("Input a starting angle or type 'random' for a random angle.")
-        choice = input("Make your choice: ")
-        try:
-                choice = int(choice)
-                while choice >= 360:
-                        choice = choice - 360
-                move(choice)
-        except ValueError:
+        choice = get_angle()
+        move(choice)
+                
+
+def get_angle():
+        proceed = False
+        while proceed == False:
+                choice = input()
                 if choice == "random":
                         choice = random.randint(0,360)
                         print("Your random angle is", choice, ".")
-                        move(choice)
+                        proceed = True
+                        return choice
                 else:
-                        print("You need to input a number or 'random.'")
-                        start()
-                        
-def turn():
-        choice = input("Input an amount of degrees to turn or type 'random': ")
-        try: 
-                choice = int(choice)
-                while choice > 360:
-                        choice = choice - 360
-                while choice < 0:
-                        choice = choice + 360
-                return choice
-        except ValueError:
-                        if choice == "random":
-                                choice = random.randint(0,360)
-                                print("Your random angle is ", choice, ".")
-                                return choice 
-                        else:
+                        try:
+                                choice = int(choice)
+                                while choice >= 360:
+                                        choice = choice - 360
+                                while choice < 0:
+                                        choice = choice + 360
+                                proceed = True
+                                return choice
+                        except ValueError:
+                                choice = 0
                                 print("You need to input a number or 'random.'")
+                                print("Input an angle or 'random': ",end="")
+                                
+def get_distance(angle):
+        proceed = False
+        while proceed == False:
+                try:
+                        distance = int(input("Input distance moved: "))
+                        proceed = True
+                except ValueError:
+                        print("You need to input an integer.")
+        radians = math.radians(angle)
+        distanceX = float(distance) * math.cos(radians)
+        distanceY = float(distance) * math.sin(radians)
+        return distanceX,distanceY
         
 def move(choice):
         angle = choice
@@ -56,25 +60,18 @@ def move(choice):
                 print("5) Quit")
                 choice = input("Make your choice: ")
                 if choice == "1":
-                        add_angle = turn()
+                        print("Input an amount of degrees to turn or type 'random': ",end="")
+                        add_angle = get_angle()
                         angle = angle + add_angle
-                        while angle > 360:
+                        while angle >= 360:
                                 angle = angle - 360
                         while angle < 0:
                                 angle = angle + 360
                         print("Your angle is now",angle,"degrees.")
                 elif choice == "2":
-                        distance = input("How far will you walk: ")
-                        try:
-                                int(distance)
-                                radians = math.radians(angle)
-                                distanceX = float(distance) * math.cos(radians)
-                                distanceY = float(distance) * math.sin(radians)
-                                positionX = positionX + distanceX
-                                positionY = positionY + distanceY
-                                print("Your coordinates are (",positionX,",",positionY,").")
-                        except ValueError:
-                                print("You must input an integer distance.")
+                        distanceX,distanceY = get_distance(angle)
+                        positionX,positionY = positionX+distanceX,positionY+distanceY
+                        print("Your coordinates are (",positionX,",",positionY,").")
                 elif choice == "3":
                         print("Your coordinates are (",positionX,",",positionY,").")
                         print("Your angle is",angle,"degrees.")
@@ -85,14 +82,17 @@ def move(choice):
                         sys.exit()
                 else:
                         enrage = enrage + 1
-                        if enrage < 3:
-                                print("Enter a number from 1 to 5.")
-                        elif enrage >= 3 and enrage < 5:
-                                print("I said ENTER A NUMBER FROM 1 TO 5.")
-                        elif enrage >=5 and enrage < 10:
-                                print("YOU INCOMPETENT @%#$ ENTER A NUMBER FROM 1 TO 5!")
-                        else:
-                                print("AJHLBNKJSAIJAWADASGTASMLAMAQWUOIEUOAKNBLADUYTAUSHDLASGBALKSJDGASKYHFVHBASJKDBASKJBVASLDYIASFAUWGHALKBHABKLVAJSNXCASUOIGALJKSRJ")
-        main()
+                        pepe_the_frog(enrage)
+                        
+def pepe_the_frog(enrage):
+        if enrage < 3:
+                print("Enter a number from 1 to 5.")
+        elif enrage >= 3 and enrage < 5:
+                print("I said ENTER A NUMBER FROM 1 TO 5.")
+        elif enrage >=5 and enrage < 10:
+                print("YOU INCOMPETENT @%#$ ENTER A NUMBER FROM 1 TO 5!")
+        else:
+                print("RRRRRRREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+        start()
         
-main()
+start()
